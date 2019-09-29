753fe0c1b0399c28074a61a7e531243c8c195720
onelrdm <onelrdm@outlook.com>
2019-09-29 14:42:24 +0800

    fix unit tests

--- config.go
+++ config.go
@@ -3,16 +3,8 @@ package reflectutil
 // Config customize how the API should behave.
 // The API is created from Config by Froze.
 type Config struct {
-	IndentionStep                 int
-	MarshalFloatWith6Digits       bool
-	EscapeHTML                    bool
-	SortMapKeys                   bool
-	UseNumber                     bool
-	DisallowUnknownFields         bool
-	TagKey                        string
-	TaggedFieldOnly               bool
-	ObjectFieldMustBeSimpleString bool
-	CaseSensitive                 bool
+	TagKey          string
+	TaggedFieldOnly bool
 }
 
 func (cfg *Config) getTagKey() string {
--- extension/xlsxutil/int_encoder_test.go
+++ extension/xlsxutil/int_encoder_test.go
@@ -1,17 +1,17 @@
 package xlsxutil
 
 import (
-	"bytes"
 	"github.com/modern-go/reflect2"
 	"github.com/onelrdm/conv"
 	"github.com/stretchr/testify/assert"
+	"github.com/tealeg/xlsx"
 	"testing"
 )
 
 func TestIntCodec_Encode(t *testing.T) {
 	v := 123
 	codec := IntCodec{}
-	var buf bytes.Buffer
-	codec.Encode(reflect2.PtrOf(v), &buf)
-	assert.Equal(t, conv.MustString(v), buf.String())
+	w := CellWriter{Cell: &xlsx.Cell{}}
+	codec.Encode(reflect2.PtrOf(v), &w)
+	assert.Equal(t, conv.MustString(v), w.String())
 }
